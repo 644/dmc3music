39029df877cf86864159880561a46c6e50b56f88
BraydonKains <kainsbraydon@gmail.com>
2021-08-20 11:04:48 -0400

    Wrapped NAudio functionality in class, use Winform timer to check room every 250 ms

--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NVorbis;
-using NAudio;
 using NAudio.Wave;
-using NAudio.Wave.SampleProviders;
 using System.Windows.Forms;
-using System.Threading;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
-using Microsoft.PowerBI.Api.Models;
 using System.Collections;
 
 namespace dmc3music
@@ -29,56 +17,55 @@ namespace dmc3music
         [DllImport("kernel32", SetLastError = true)]
         public static extern int ReadProcessMemory(IntPtr hProcess, int lpBase, ref int lpBuffer, int nSize, int lpNumberOfBytesRead);
 
+        private Timer SongChangeTimer { get; set; }
+
+        private SongPlayer Player { get; set; }
+
+        private Process DMC3Process { get; set; }
+        private IntPtr ProcessHandle { get; set; }
+        private int BaseAddress { get; set; }
+
         public Form1()
         {
             InitializeComponent();
+            Player = new SongPlayer();
         }
 
         public static class Globals
         {
-            public static WaveOut waveOut = new NAudio.Wave.WaveOut();
+            public static WaveOut waveOut = new WaveOut();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Hashtable songTable = new Hashtable(){
-                {0, "Battle_00.ogg"},
-                {1, "Battle_01.ogg"},
-                {2, "Battle_01.ogg"},
-                {6, "Boss_01.ogg"}
-            };
-            Process process = Process.GetProcessesByName("dmc3se")[0];
-            IntPtr processHandle = OpenProcess(PROCESS_WM_READ, false, process.Id);
-            int baseAddress = process.MainModule.BaseAddress.ToInt32();
-            bool isPlaying = false;
-            int playSong = 0;
-            int roomId = 0;
-
-            while (true)
-            {
-                ReadProcessMemory(processHandle, baseAddress + 0x20C39EC, ref playSong, sizeof(int), 0);
-                if(playSong == 1 && !isPlaying)
-                {
-                    isPlaying = true;
-                    ReadProcessMemory(processHandle, baseAddress + 0x76B150, ref roomId, sizeof(int), 0);
-                    var track = songTable[roomId];
-                    Console.WriteLine(track);
-                    var vorbis = new NAudio.Vorbis.VorbisWaveReader(@"D:\SteamLibrary\steamapps\common\Devil May Cry 3\native\sound\" + track);
-                    Globals.waveOut.Init(vorbis);
-                    Globals.waveOut.Play();
-                }
-                else if(playSong == 0 && isPlaying)
-                {
-                    isPlaying = false;
-                    Globals.waveOut?.Stop();
-                }
-                System.Threading.Thread.Sleep(250);
-            }
+            DMC3Process = Process.GetProcessesByName("dmc3se")[0];
+            ProcessHandle = OpenProcess(PROCESS_WM_READ, false, DMC3Process.Id);
+            BaseAddress = DMC3Process.MainModule.BaseAddress.ToInt32();
+            SongChangeTimer = new Timer();
+            SongChangeTimer.Interval = 250;
+            SongChangeTimer.Tick += new EventHandler(CheckSong);
+            SongChangeTimer.Start();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Globals.waveOut?.Stop();
+            SongChangeTimer.Stop();
+            Player.Stop();
+        }
+
+        private void CheckSong(object sender, EventArgs e)
+        {
+            int checkRoom = 0;
+            ReadProcessMemory(ProcessHandle, BaseAddress + 0x20C39EC, ref checkRoom, sizeof(int), 0);
+            if (checkRoom == 0)
+            {
+                Player.Stop();
+                return;
+            }
+
+            int roomId = -1;
+            ReadProcessMemory(ProcessHandle, BaseAddress + 0x76B150, ref roomId, sizeof(int), 0);
+            Player.PlayRoomSong(roomId);
         }
     }
 }
